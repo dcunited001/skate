afbe32e8ec003e494fb76edfe4314d06d08982eb
David Conner <dconner.pro@gmail.com>
2011-08-03 19:21:21 -0400

    changed location of devise constants and added it's file to the environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+require File.expand_path('../../lib/devise_names', __FILE__)
 require File.expand_path('../../lib/guest/guest_app', __FILE__)
 
 # Initialize the rails application
---
+++ lib/devise_names.rb
@@ -0,0 +1,20 @@
+class DeviseNames
+
+  #constants for devise login
+  DEVISE_LINKS_CLASS = '.login'
+  DEVISE_FORM_CLASS = '.login-form'
+  DEVISE_FORM_ID = '.login-form'
+
+  SIGN_IN_LINK_TEXT = 'Log In'
+  SIGN_IN_LINK_ID = '#login-link'
+
+  SIGN_OUT_LINK_TEXT = 'Log Out'
+  SIGN_OUT_LINK_ID = '#logout-link'
+
+  SIGN_UP_LINK_TEXT = 'Sign Up'
+  SIGN_UP_LINK_ID = '#signup-link'
+
+  REGISTER_LINK_TEXT = 'Register'
+  REGISTER_LINK_ID = '#register'
+
+end
