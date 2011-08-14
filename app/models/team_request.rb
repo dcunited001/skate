fb8f25fbc1d27c7c6cbe6c9043ac3859758d158c
David Conner <dconner.pro@gmail.com>
2011-08-14 03:30:28 -0400

    added team request scopes

--- app/models/team_request.rb
+++ app/models/team_request.rb
@@ -5,12 +5,14 @@ class TeamRequest < ActiveRecord::Base
   belongs_to :team
 
   scope :for_team, lambda { |team| where(:team_id => team.id) }
-
   scope :sent_from, lambda { |member| where(:member_requesting_id => member.id) }
   scope :sent_to,  lambda { |member| where(:member_requested_id => member.id) }
 
-
-
+  scope :pending, where('active = ? and accepted = ? and rejected = ?', false, false, false)
+  scope :active, where('active = ?', true)
+  scope :accepted, where('accepted = ?', true)
+  scope :rejected, where('rejected = ?', true)
+  scope :ended, where('accepted = ? and rejected = ?', false, true)
 
   # incoming/outgoing requests (from the team's perspective')
   scope :incoming, where('incoming = ?', true)
